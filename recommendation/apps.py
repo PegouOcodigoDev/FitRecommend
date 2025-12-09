9a6ac7a197b7bd4293ed0ac5617df9d61757b8df
Eduardo <eduardojunior010757@gmail.com>
2025-12-09 17:14:42 -0300

    Add admin superuser creation in RecommendationConfig and update app reference in settings

--- recommendation/apps.py
+++ recommendation/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
+from django.contrib.auth import get_user_model
 
 
 class RecommendationConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "recommendation"
+
+    def ready(self):
+        User = get_user_model()
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser('admin', '', 'admin')
--- workout_project/settings.py
+++ workout_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "recommendation",
+    "recommendation.apps.RecommendationConfig",
 ]
 
 MIDDLEWARE = [
